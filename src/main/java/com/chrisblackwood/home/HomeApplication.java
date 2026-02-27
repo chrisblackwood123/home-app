d5b92024f4fdc31b2bae939c6e12320838a4193a
Chris Blackwood <chris.blackwood20@gmail.com>
2026-02-27 20:35:31 +0000

    feat: implement command runner

--- src/main/java/com/chrisblackwood/home/HomeApplication.java
+++ src/main/java/com/chrisblackwood/home/HomeApplication.java
@@ -1,15 +1,26 @@
 
 package com.chrisblackwood.home;
 
+import com.chrisblackwood.home.service.NotificationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
-public class HomeApplication {
+public class HomeApplication implements CommandLineRunner {
+
+	private final NotificationService notificationService;
+
+    public HomeApplication(NotificationService notificationService) {
+		this.notificationService = notificationService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		notificationService.sendNotification();
 	}
 }
